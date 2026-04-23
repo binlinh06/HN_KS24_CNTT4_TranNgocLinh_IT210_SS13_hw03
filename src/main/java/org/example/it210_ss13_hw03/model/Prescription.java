af8818c2d60710a53f1d7be41b102a683686e430
binlinh06 <nglinh150606@gmail.com>
2026-04-23 07:28:52 +0700

    Update Prescription.java

--- src/main/java/org/example/it210_ss13_hw03/model/Prescription.java
+++ src/main/java/org/example/it210_ss13_hw03/model/Prescription.java
@@ -1,4 +1,26 @@
 package org.example.it210_ss13_hw03.model;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "prescriptions")
 public class Prescription {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String patientName;
+
+    @OneToMany(
+            mappedBy = "prescription",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY,
+            orphanRemoval = true
+    )
+    private List<PrescriptionDetail> details;
+
+    // getter/setter
 }
