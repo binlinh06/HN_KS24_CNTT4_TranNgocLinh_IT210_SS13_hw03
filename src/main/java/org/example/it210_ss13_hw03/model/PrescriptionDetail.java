02018b820e57a9a965cbdc0411100a779f4418fa
binlinh06 <nglinh150606@gmail.com>
2026-04-23 07:28:32 +0700

    Update PrescriptionDetail.java

--- src/main/java/org/example/it210_ss13_hw03/model/PrescriptionDetail.java
+++ src/main/java/org/example/it210_ss13_hw03/model/PrescriptionDetail.java
@@ -1,4 +1,21 @@
 package org.example.it210_ss13_hw03.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "prescription_details")
 public class PrescriptionDetail {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String medicineName;
+    private int quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "prescription_id")
+    private Prescription prescription;
+
+    // getter/setter
 }
